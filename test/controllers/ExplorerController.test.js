6dd98316c6ee2a419578c0ebb02d65767d7b0ac4
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-10 01:05:47 -0700

    fizzbuzz feature

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,25 +1,32 @@
 const ExplorerController = require("../../lib/controllers/ExplorerController");
 
 describe("Tests para ExplorerController", () => {
-    test("Requerimiento 1: Filtrar explorers por misión", () => {
-        const explorers = [{mission: "node"}, {mission: "java"}];
+    test("1. Filtrar explorers por misión", () => {
         const explorersInNode = ExplorerController.getExplorersByMission("node");
         expect(explorersInNode.length).toBe(10);
-        expect(explorersInNode[0].name).toBe("Woopa1")
-        expect(explorersInNode[0].score).toBe(1)
-        expect(explorersInNode[9].name).toBe("Woopa15")
-        expect(explorersInNode[9].score).toBe(15)
+        expect(explorersInNode[0].name).toBe("Woopa1");
+        expect(explorersInNode[0].score).toBe(1);
+        expect(explorersInNode[9].name).toBe("Woopa15");
+        expect(explorersInNode[9].score).toBe(15);
     }),
-    test("Requerimiento 2: Cantidad de explorers en misión", () => {
-        const explorers = [{mission: "node"}, {mission: "java"}];
+    test("2. Cantidad de explorers en misión", () => {
         const numexplorersInNode = ExplorerController.getExplorersAmountByMission("node");
         expect(numexplorersInNode).toBe(10);
     }),
-    test("Requerimiento 3: Username de explorers en misión", () => {
-        const explorers = [{githubUsername: "user1", mission: "node"}, {githubUsername: "user2", mission: "java"}];
+    test("3. Username de explorers en misión", () => {
         const usernamesInNode = ExplorerController.getExplorersUsernamesByMission("node");
         expect(usernamesInNode.length).toBe(10);
-        expect(usernamesInNode[0]).toBe("ajolonauta1")
-        expect(usernamesInNode[9]).toBe("ajolonauta15")
+        expect(usernamesInNode[0]).toBe("ajolonauta1");
+        expect(usernamesInNode[9]).toBe("ajolonauta15");
+    }),
+    test("4. Validar FizzBuzzTrick de un número", () => {
+        const trick3 = ExplorerController.validateFizzBuzzTrick(3);
+        expect(trick3).toBe("FIZZ");
+        const trick5 = ExplorerController.validateFizzBuzzTrick(5);
+        expect(trick5).toBe("BUZZ");
+        const trick15 = ExplorerController.validateFizzBuzzTrick(15);
+        expect(trick15).toBe("FIZZBUZZ");
+        const trick1 = ExplorerController.validateFizzBuzzTrick(1);
+        expect(trick1).toBe(1);
     });
 });
